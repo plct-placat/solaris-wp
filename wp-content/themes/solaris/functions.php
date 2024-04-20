6ca88a7a2e1289719ca1f5ed05833826da03064d
Nazarov-Dmitriy <Difat1@inbox.ru>
2024-04-20 19:37:58 +0300

    function

--- wp-content/themes/solaris/functions.php
+++ wp-content/themes/solaris/functions.php
@@ -4,6 +4,6 @@ function solaris_scripts()
 {
 	wp_enqueue_style('solaris-style', get_stylesheet_uri());
 	wp_enqueue_script('solaris-scripts', get_template_directory_uri() . '/assets/js/script.js', array(), null, true);
-	}
+}
 add_action('wp_enqueue_scripts', 'solaris_scripts');
 ?>
